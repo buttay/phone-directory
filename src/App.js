cc292832e9b2f038ec06f3f369a49e878f636eab
Ankita Gupta <sweetankitagupta4@gmail.com>
2024-02-02 14:55:55 +0530

    Displayed Phone Directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 import { Component } from 'react';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-  
+        Phone Directory
       </div>
     );
   }
