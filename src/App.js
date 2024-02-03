9dded536d3e663a6111e6369fbcec0f4d6e06639
Ankita Gupta <sweetankitagupta4@gmail.com>
2024-02-03 11:09:07 +0530

    Created ‘Header’ Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import { Component } from 'react';
+import Header from './Header';
 
 class App extends Component {
+
+
   render() {
     return (
-      <div className="App">
-        Phone Directory
-      </div>
+      <>
+        <Header />
+        <button>Add</button><div>
+          <span>Name</span><br />
+          <span>Phone</span>
+        </div>
+      </>
     );
   }
-  
+
 }
 
 export default App;
---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+const Header = function () {
+  return (
+    < div className='header' >
+      Phone Directory
+    </div >
+  )
+};
+
+// export class Header extends Component {
+//   render() {
+//     return {
+//       < div className = 'header' >
+//       Phone Directory
+//         </div >
+//     }
+// }
+// }
+export default Header;
